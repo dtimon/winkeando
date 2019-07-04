497cdeb9cea0cb778e9853a8b8001176c36a0a73
Timón Morillo Velarde, David <david.timon@gmail.com>
2019-07-04 23:40:05 +0200

    refactor(gasp): add gasp for animation

--- packages/wk-product/src/wk-product.js
+++ packages/wk-product/src/wk-product.js
@@ -1,16 +1,19 @@
 // Import the LitElement base class and html helper function
 import { LitElement, html, css } from 'lit-element';
+import { TweenMax, TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
 
 // Extend the LitElement base class
 class WkProduct extends LitElement {
   static get properties() {
     return {
+      img: String,
       title: String,
       desc: String,
       summary: String,
       price: {
         converter: {
           fromAttribute: (value, type) => {
+
             // `value` is a string
             // Convert it to a value of type `type` and return it
             let valueObject = JSON.parse(value);
@@ -25,17 +28,17 @@ class WkProduct extends LitElement {
     return css`
       .product {
         z-index: 0;
-        position: absolute;
+        position: relative;
         color: red;
       }
       .title, .desc {
         z-index: 1;
-        position: absolute;
+        position: relative;
         
       }
       .summary {
         z-index:2;
-        position: absolute;
+        position: relative;
         
       }
       .price {
@@ -64,16 +67,30 @@ class WkProduct extends LitElement {
     return html`
       <!-- template content -->
       <div class="product">
-        <div class="title">${this.title}</div>
+        <img class="image" src="${this.img}"></img>
+        <div class="title">${this.title} ${this.img}</div>
         <div class="desc">${this.desc}</div>
         <div class="summary">${this.summary}</div>
-        <div class="price" style="height: 150px; width: 150px;background: url('xxxxxxxxxxxxxxxxxx')">
+        <div class="price">
           <span class="amount">${this.price.amount}</span>
           <span class="currency"> ${this.price.currency}</span>
         </div>
       
       </div>
     `;
+  }
+
+  firstUpdated() {
+    super.firstUpdated();
+    const title = this.renderRoot.querySelector('.title');
+    const desc = this.renderRoot.querySelector('.desc');
+    const summary = this.renderRoot.querySelector('.summary');
+    const price = this.renderRoot.querySelector('.price');
+    const image = this.renderRoot.querySelector('.image');
+
+    TweenMax.to([title, desc], 2, { x: 200, y: 600, width: "200px", height: "150px" });
+    TweenMax.to([price], 2, { x: "20%", y: 0, width: "300px", height: "600px" });
+    TweenMax.to([image], 2, { x: 0, y: 0, width: "300px", height: "600px" });
   }
 }
 // Register the new element with the browser.
